9998a01d175f10e87072381dc452a3ae55fd712d
neigiamasJonas <p.ezerskis@gmail.com>
2022-04-27 16:21:31 +0300

    header lvl4 js media

--- js/pages/home.js
+++ js/pages/home.js
@@ -35,6 +35,22 @@ menuBtn.addEventListener('click', () => {
 
 });
 
+// accordion
+let accordions = document.getElementsByClassName('nav-link');
+
+for (let i = 0; i < accordions.length; i++) {
+
+    accordions[i].onclick = function () {
+        let dropList = this.nextElementSibling;
+
+        if (dropList.style.maxHeight) {
+            dropList.style.maxHeight = null;
+        } else {
+            dropList.style.maxHeight = dropList.scrollHeight + 'px';
+        }
+    }
+}
+
 /* header: end */
 
 /* hero: start */
